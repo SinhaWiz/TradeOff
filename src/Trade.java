65c04d089779c2a5db1cb0fe5ede2104c6963c9c
Navid Ibrahim <nibrahim7787@gmail.com>
2024-11-02 11:12:32 +0600

    Created Trade class having long & short trading methods

--- src/Trade.java
+++ src/Trade.java
@@ -1,4 +1,29 @@
 package src;
 
 public class Trade {
+    private double quantity;
+    private double coinPrice;
+    private final double tradingAmount = quantity * coinPrice;
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public double getCoinPrice() {
+        return coinPrice;
+    }
+
+    public double getTradingAmount() {
+        return tradingAmount;
+    }
+
+    public double longTrade(double initialAmount, double percentageChange, double leverage) {
+        double leveragedChange = percentageChange * leverage;
+        return initialAmount * (1 + leveragedChange / 100);
+    }
+
+    public double shortTrade(double initialAmount, double percentageChange, double leverage) {
+        double leveragedChange = percentageChange * leverage;
+        return initialAmount * (1 - leveragedChange / 100);
+    }
 }
