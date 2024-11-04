8ed0ba87a509d406dfafbc9cb9a1f1d064e0a97a
tahirdotz <tahirzaman075@gmail.com>
2024-11-04 22:36:56 +0600

    Deleting unnecessary sout

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 package src;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is the test of main class");
         GameController gameController = new GameController();
         gameController.startGame();
 
