70574d7613f920a528688edcee00739b91b0ed3d
SinhaWiz <sinhartx2070@gmail.com>
2024-11-17 08:24:12 +0600

    UI update For Market Table

--- src/MarketTableGenerator.java
+++ src/MarketTableGenerator.java
@@ -5,15 +5,16 @@ import java.util.List;
 public class MarketTableGenerator {
     public void displayTable(List<Coin> coins) {
         System.out.println("\n=== Current Market Prices ===");
-        System.out.println(String.format("%-15s %-10s %-15s", "Coin", "Ticker", "Price"));
-        System.out.println("----------------------------------------");
+        System.out.println(String.format("%-15s %-10s %-15s", "|    Coin", "|   Ticker  |", "    Price     |"));
+        System.out.println("|___________________________________________|");
 
         for (Coin coin : coins) {
-            System.out.printf("%-15s %-10s $%-15.2f\n",
+            System.out.printf("|%-15s %-10s $%-15.2f" +"|"+
+                            "\n",
                     coin.getName(),
                     coin.getTicker(),
                     coin.getPrice());
         }
-        System.out.println("----------------------------------------");
+        System.out.println("|___________________________________________|");
     }
 }
