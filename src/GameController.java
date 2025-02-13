24206535a7b93d5c8251b8d484b1f630e36037ec
Navid Ibrahim <nibrahim7787@gmail.com>
2025-02-13 21:30:43 +0600

    Fixed a typo

--- src/GameController.java
+++ src/GameController.java
@@ -53,16 +53,9 @@ public class GameController {
     private void displayMenu() {
 
         System.out.println("\n     |$|$|$|$| |$|$|$|$| |$|$|$|$| |$|$|$|$| |$|$|$|$| MENU |$|$|$|$| |$|$|$|$| |$|$|$|$| |$|$|$|$| |$|$|$|$|");
-        System.out.println("|+|+|+| 1.View Market        |+|+|+| 2.View Portfolio|+|+|+| 3.Close Position|+|+|+| 4.Open Long Position|+|+|+|");
-        //System.out.println();
-      //  System.out.println("");
-
-        System.out.println("|+|+|+| 5.Open Short Position|+|+|+| 6. Skip Turn    |+|+|+| 7.Skip a day    |+|+|+| 8. Exit             |+|+|+|");
-
-        //System.out.println("");
-        //System.out.println("");
-        //System.out.println("");
-       // System.out.println("");
+        System.out.println("|+|+|+| 1.View Market        |+|+|+| 2.View Portfolio|+|+|+| 3.Close Position|+|+|+| 4.Open Long Position             |+|+|+|");
+        System.out.println("|+|+|+| 5.Open Short Position|+|+|+| 6.Skip Turn     |+|+|+| 7.Skip a day    |+|+|+| 8.Bribe Market Insiders ($5000)  |+|+|+|");
+        System.out.println("|+|+|+| 9.Exit Game          |+|+|+|");
         System.out.println("\nBalance: $" + String.format("%.2f", player.getBalance()));
     }
 
