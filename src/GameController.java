134d887d475127895b83f045e28bcfd1bc361286
SinhaWiz <sinhartx2070@gmail.com>
2025-05-04 07:26:29 +0600

    removed some strings from GiantClass

--- src/GameController.java
+++ src/GameController.java
@@ -36,9 +36,10 @@ public class GameController {
 
     public void displayStartMenu() {
         displayWelcomeMessage();
-        System.out.println("1. Start New Game");
-        System.out.println("2. Load Game");
-        System.out.println("3. Exit");
+        DisplayString.mainMenuOptions();
+        mainMenuInputHandle();
+    }
+    public void  mainMenuInputHandle(){
         while (true) {
             System.out.print("Enter your choice: ");
             try {
@@ -67,9 +68,8 @@ public class GameController {
             }
         }
     }
-
     public void startGame() {
-        while (turnsRemaining > 0) {
+        while (turnsRemaining>0) {
             System.out.println("\nTurns remaining: " + turnsRemaining);
             displayMenu();
             boolean turnUsed = handleAction();
@@ -113,28 +113,17 @@ public class GameController {
     }
 
     public void displayWelcomeMessage() {
-        System.out.println("\n|$|$|$|$|$|$|$|$|$|$|$|$|$|$|$|$|$|$|$| Welcome to TradeOff |$|$|$|$|$|$|$|$|$|$|$|$|$|$|$|$|$|$|\n");
-        System.out.println("You are broke and borrowed $1,000,000 from Loan Sharks.");
-        System.out.println("You have to pay them $3,000,000 within 10 days.");
-        System.out.println("If you don't, the consequences will be beyond your imagination.");
-        System.out.println("Because of this short deadline, you have opted for cryptocurrency trading to earn this money ASAP.\n");
-        System.out.println("You have " + MAX_TURNS + " turns to make your fortune.");
+      DisplayString.welcomeMessage();
     }
 
     public void displayMenu() {
-        System.out.println("\n     |$|$|$|$| |$|$|$|$| |$|$|$|$| |$|$|$|$| |$|$|$|$| MENU |$|$|$|$| |$|$|$|$| |$|$|$|$| |$|$|$|$| |$|$|$|$|");
-        System.out.println("|+|+|+| 1.View Market         |+|+|+| 2.View Portfolio |+|+|+| 3.Close Position       |+|+|+| 4.Open Long Position             |+|+|+|");
-        System.out.println("|+|+|+| 5.Open Short Position |+|+|+| 6.Skip Turn      |+|+|+| 7.Skip a Day           |+|+|+| 8.Consult Market Insider  (" + marketInsiderAttempts + "/3)  |+|+|+|");
-        System.out.println("|+|+|+| 9.Statistics          |+|+|+| 10.Save Game     |+|+|+| 11.Load Game           |+|+|+| 12.Exit Game                     |+|+|+|");
-        System.out.println("\nBalance: $" + String.format("%.2f", player.getBalance()));
+       DisplayString.secondaryMenuOptions(marketInsiderAttempts, player.getBalance());
     }
 
     private boolean handleAction() {
         System.out.print("Enter your choice: ");
         try {
-
             int choice = scanner.nextInt();
-
             switch (choice) {
                 case 1:
                     clearConsole();
@@ -199,19 +188,13 @@ public class GameController {
         if (marketInsiderAttempts > 0) {
             int currentPrice = 75000;
 
-            if (marketInsiderAttempts == 3) {
-                currentPrice = 75000;
-            } else if (marketInsiderAttempts == 2) {
+            if (marketInsiderAttempts == 2) {
                 currentPrice = 100000;
             } else if (marketInsiderAttempts == 1) {
                 currentPrice = 150000;
             }
-
             if (player.getBalance() >= currentPrice) {
-                System.out.println("WARNING: Consulting the market insider is a dangerous and costly activity!");
-                System.out.println("His consulting cost is $" + currentPrice);
-                System.out.println("If you get caught, you will be sent to jail and 20 turns will be skipped.");
-                System.out.println("Do you wish to proceed? (y/n)");
+                DisplayString.blackMarketWarning(currentPrice);
                 Scanner scanner = new Scanner(System.in);
                 String decision = scanner.next().toLowerCase();
 
@@ -236,12 +219,7 @@ public class GameController {
                         }
                         System.out.println("Attempts left: " + marketInsiderAttempts);
                     } else if (marketInsiderAttempts == 1) {
-
-                        System.out.println("Oh no! You have been caught by the authorities!");
-                        System.out.println("You will be in jail for 20 turns. The market will continue to move during this time.");
-                        System.out.println("Or you can pay your bail: $" + currentPrice * 2);
-                        System.out.println("Press 0 to pay bail, Press 1 to go to jail");
-
+                        DisplayString.caughtInAction(currentPrice);
                         int decision2StayOut = scanner.nextInt();
                         if (decision2StayOut == 1) {
                             for (int i = 0; i < 20; i++) {
@@ -617,28 +595,13 @@ public class GameController {
         System.out.println("Final Balance: $" + String.format("%.2f", finalBalance));
         System.out.println("Initial Balance: $1,000,000.00");
         System.out.println("Profit/Loss: $" + String.format("%.2f", finalBalance - 1000000));
-
         // Enhanced ending based on the player's final balance
         if (finalBalance >= 3000000) {
-            System.out.println("\n=== Congratulations! ===");
-            System.out.println("You've done the impossible! Not only did you pay off the loan sharks, but you also made a massive profit.");
-            System.out.println("The loan sharks are impressed by your skills and even offer you a partnership in their 'legitimate' business ventures.");
-            System.out.println("You walk away with your head held high, knowing you've beaten the odds and secured your future.");
-            System.out.println("But remember... the crypto world is unpredictable. Will you stay in the game, or cash out and live the good life?");
-            System.out.println("The choice is yours.");
+            DisplayString.winningMessage();
         } else if (finalBalance >= 1000000) {
-            System.out.println("Sure, you've managed to break even and perhaps made some more money on the side.");
-            System.out.println("But did you take a good look at your balance again?");
-            System.out.println("Thought so, that definitely doesn't say 3 million...");
-            System.out.println("If you get lucky, maybe they'll let you off the hook...");
-            System.out.println("If...");
+            DisplayString.noLossNoProfitMessage();
         } else {
-            System.out.println("\n=== Game Over! ===");
-            System.out.println("The loan sharks have come to collect their dues... and you don't have enough.");
-            System.out.println("They take everything you have, leaving you with nothing but the clothes on your back.");
-            System.out.println("As they drag you away, you realize the true cost of gambling with borrowed money.");
-            System.out.println("The crypto world is unforgiving, and now you're paying the ultimate price.");
-            System.out.println("Better luck next time... if there is a next time.");
+         DisplayString.losingMessage();
         }
 
         System.out.println("\n=== Final Portfolio ===");
@@ -748,7 +711,6 @@ public class GameController {
         }
     }
 
-
     public void loadGameStartMenu() {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(SAVE_FILE))) {
             double balance = in.readDouble();
@@ -770,24 +732,18 @@ public class GameController {
             e.printStackTrace();
         }
     }
-
-
     public int getTurnsRemaining() {
         return turnsRemaining;
     }
-
     public void setTurnsRemaining(int turnsRemaining) {
         this.turnsRemaining = turnsRemaining;
     }
-
     public Market getMarket() {
         return market;
     }
-
     public void setMarket(Market market) {
         this.market = market;
     }
-
     public Player getPlayer() {
         return player;
     }
